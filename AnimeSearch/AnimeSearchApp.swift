629bb3050e9362e0107e896c8ca7b467319e3286
fradleybrancis <fradleybrancis@gmail.com>
2020-11-17 18:21:36 -0500

    Initial Commit

---
+++ AnimeSearch/AnimeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeSearchApp.swift
+//  AnimeSearch
+//
+//  Created by Rave BizzDev on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
